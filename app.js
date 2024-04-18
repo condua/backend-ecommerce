2a287d052e00de079144d7482628ff29649d6f7c
condua <phuc.phanphuc2002@hcmut.edu.vn>
2024-04-18 15:22:22 +0700

    first commit

--- app.js
+++ app.js
@@ -30,18 +30,18 @@ app.use('/api/v1', order);
 app.use('/api/v1', payment);
 
 // deployment
-__dirname = path.resolve();
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
+// __dirname = path.resolve();
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    });
-} else {
-    app.get('/', (req, res) => {
-        res.send('Server is Running! 🚀');
-    });
-}
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+//     });
+// } else {
+//     app.get('/', (req, res) => {
+//         res.send('Server is Running! 🚀');
+//     });
+// }
 
 // error middleware
 app.use(errorMiddleware);
